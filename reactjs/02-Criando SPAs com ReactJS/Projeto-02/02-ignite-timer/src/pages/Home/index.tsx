0cc904ab9d173255ad8b91bba0ac93d3233407c3
diego-lopes <diegolopes2009@hotmail.com>
2023-01-14 13:05:38 -0300

    chore: initial new cycle

--- reactjs/02-Criando SPAs com ReactJS/Projeto-02/02-ignite-timer/src/pages/Home/index.tsx
+++ reactjs/02-Criando SPAs com ReactJS/Projeto-02/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Play } from 'phosphor-react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as zod from 'zod';
 import {
@@ -12,23 +13,45 @@ import {
   TaskInput
 } from './styles';
 
-const newCycleFormValidationSchema = zod.object({
+const newCycleFormValidationSchema = zod.object({ // criando o objeto de regras para passar no zodResolver.
   task: zod.string().min(1, "Informa a tarefa"),
-  MinutesAmount: zod.number()
-  .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
-  .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
+  minutesAmount: zod.number()
+    .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
+    .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
 })
 
+// interface NewCycleFormData {
+//   task: string;
+//   minutesAmount: number;
+// }
+
+// tipando useForm com base do objeto zod
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema> // usamos infer de infereir do zod para deixar dinâmico a tipagem
+
+
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
-    resolver: zodResolver(newCycleFormValidationSchema),
+
+  const [] = useState<>()
+
+  const { register, handleSubmit, watch, formState, reset  } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema), // integração com zod usando hookform
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
   });
 
-  function handleCreateNewCycle(data) {
+  function handleCreateNewCycle(data:NewCycleFormData) {
     console.log(data);
-    
+    reset();
   }
-  
+  console.log(formState.errors); // visualizando o error.
+
   const task = watch('task'); // observando em tempo real o valor do input
   const isSubmitDisabled = !task; // variavel auxiliar bem explicativa
   return (
@@ -36,10 +59,10 @@ export function Home() {
       <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">vou trabalhar em</label>
-          <TaskInput 
-            list="task-suggestions" 
-            id="task" 
-            placeholder="Dê um nome para o seu projetos" 
+          <TaskInput
+            list="task-suggestions"
+            id="task"
+            placeholder="Dê um nome para o seu projetos"
             {...register("task")}
           />
 
@@ -58,7 +81,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
-            {...register("MinutesAmount", { valueAsNumber: true})}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -70,7 +93,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <StartCountdownButton  disabled={isSubmitDisabled}  type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           começar
         </StartCountdownButton>
