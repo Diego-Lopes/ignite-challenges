70f7159ca4de51d600b4c32a12e01072fe839679
diego-lopes <diegolopes2009@hotmail.com>
2023-01-19 18:35:34 -0300

    chore: added reducers and enum

---
+++ reactjs/02-Criando SPAs com ReactJS/Projeto-02/02-ignite-timer/src/reducers/cycles/actions.ts
@@ -0,0 +1,27 @@
+import { Cycle } from "./reducer";
+
+export enum ActionTypes {
+  add_new_cycle = "add_new_cycle",
+  interrupt_current_cycle = "interrupt_current_cycle",
+  mark_current_finished_cycle = "mark_current_finished_cycle",
+
+}
+
+export function addNewCycleAction(newCycle: Cycle) {
+  return {
+    type: ActionTypes.add_new_cycle,
+    payload: {
+      newCycle,
+    }
+  }
+}
+export function markCurrentCycleAsFinishedAction() {
+  return {
+    type: ActionTypes.mark_current_finished_cycle
+  }
+}
+export function interruptCurrentCycleAction() {
+  return {
+    type: ActionTypes.interrupt_current_cycle
+  }
+}
---
+++ reactjs/02-Criando SPAs com ReactJS/Projeto-02/02-ignite-timer/src/reducers/cycles/reducer.ts
@@ -0,0 +1,84 @@
+import { produce } from "immer";
+import { ActionTypes } from "./actions";
+
+export interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
+}
+
+interface CyclesState {
+  cycles: Cycle[];
+  activeCycleId: string | null;
+}
+
+export function cyclesReducer(state: CyclesState, action: any) {
+  switch (action.type) {
+    case ActionTypes.add_new_cycle:
+      // return {
+      //   ...state,
+      //   cycles: [...state.cycles, action.payload.newCycle],
+      //   activeCycleId: action.payload.newCycle.id,
+      // };
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle);
+        draft.activeCycleId = action.payload.newCycle.id;
+      });
+    case ActionTypes.interrupt_current_cycle: {
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, interruptedDate: new Date() };
+      //     } else {
+      //       return cycle;
+      //     }
+      //   }),
+      //   activeCycleId: null,
+      // };
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
+
+      if (currentCycleIndex < 0) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].interruptedDate = new Date();
+      });
+    }
+    case ActionTypes.mark_current_finished_cycle: {
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, finishedDate: new Date() };
+      //     } else {
+      //       return cycle;
+      //     }
+      //   }),
+      //   activeCycleId: null,
+      // };
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
+
+      if (currentCycleIndex < 0) {
+        return state;
+      }
+
+      return produce(state, draft => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].finishedDate = new Date();
+      })
+    }
+
+    default:
+      return state;
+  }
+}
