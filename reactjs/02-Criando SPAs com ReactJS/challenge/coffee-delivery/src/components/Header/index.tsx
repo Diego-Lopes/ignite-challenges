136699564b9d7a631712f9dde1ada4336508b851
Diego-Lopes <diegolopes2009@hotmail.com>
2023-01-29 23:23:00 +0044

    chore: modification in components

--- reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/components/Header/index.tsx
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/components/Header/index.tsx
@@ -1,9 +1,19 @@
 import * as S from './styles';
+import { MapPin, ShoppingCart } from 'phosphor-react';
 import logo from '../../../src/assets/Logo.svg';
 export function Header() {
   return (
     <S.HeaderContainer>
       <img src={logo} alt="Logo Coffe Delivery" />
+      <S.BoxButons>
+        <S.ButtonLocal>
+          <MapPin size={22} weight="fill" />
+          Ji-Paraná/RO
+        </S.ButtonLocal>
+        <S.ButtonCheckout>
+          <ShoppingCart size={22} weight="fill" />
+        </S.ButtonCheckout>
+      </S.BoxButons>
     </S.HeaderContainer>
   )
 }
--- reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/pages/Home/index.tsx
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
+import { Banner } from "../../components/Banner";
 import * as S from "./styles";
 
 export function Home() {
 
   return (
     <S.HomeContainer>
-      <h1>Hello World</h1>
+      <Banner />
     </S.HomeContainer>
   )
 }
