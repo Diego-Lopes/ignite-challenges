ea74b9ba0384a30dee0ce4e9dff5b1d3f938c24e
Diego-Lopes <diegolopes2009@hotmail.com>
2023-02-06 22:04:51 +0044

    style: modification styles in banner

--- reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/components/Banner/styles.ts
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/components/Banner/styles.ts
@@ -11,9 +11,14 @@ export const BannerContainer = styled.div`
 export const Content = styled.div`
   width: min(112rem, 100%);
   margin-inline: auto;
+
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
 `;
 
 export const WrapperTexts = styled.div`
+  width: 50%;
   .title {
     font-family: "Baloo 2", sans-serif;
     font-weight: 800;
@@ -28,4 +33,38 @@ export const WrapperTexts = styled.div`
     line-height: 26px;
     color: ${(props) => props.theme["base-subtitle"]};
   }
+
+  .boxIcons {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 2rem 4rem;
+
+    .icon {
+      width: 45%;
+      display: flex;
+      align-items: center;
+      gap: 1.2rem;
+
+      p {
+        /* width: 18.7rem; */
+        font-size: 1.6rem;
+        line-height: 20.8px;
+        color: ${props => props.theme["base-text"]};
+      }
+    }
+  }
+`;
+
+export const WrapperImg = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  
+  img {
+    width: 476px;
+    height: 360px;
+  }
+
+
 `;
