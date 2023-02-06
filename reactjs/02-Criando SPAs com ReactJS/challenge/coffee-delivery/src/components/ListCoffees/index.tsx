5229738af5a09cf1ddda7859f04ad47203696860
Diego-Lopes <diegolopes2009@hotmail.com>
2023-02-06 22:06:04 +0044

    feat: added new component listCoffees

---
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/components/ListCoffees/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles';
+
+
+export function ListCoffees() {
+
+  return (
+    <S.ListCoffeeContainer>
+      <h1>List Coffees here.</h1>
+    </S.ListCoffeeContainer>
+  )
+}
---
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/components/ListCoffees/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const ListCoffeeContainer = styled.div`
+    width: 100%;
+
+    h1 {
+      width: min(112rem, 100%);
+      margin-inline: auto;
+    }
+
+
+
+
+`;
