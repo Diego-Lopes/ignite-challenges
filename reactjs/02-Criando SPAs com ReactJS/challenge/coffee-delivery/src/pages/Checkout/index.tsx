bb3d6e0bb64bd0663c319bb189d9c86e9e8a378b
Diego-Lopes <diegolopes2009@hotmail.com>
2023-02-14 22:52:19 +0044

    feat: added new component page checkout

---
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/pages/Checkout/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles';
+
+export function Checkout() {
+  return (
+    <S.CheckoutContainer>
+      <S.Content>
+        <h1>It's page checkout</h1>
+      </S.Content>
+    </S.CheckoutContainer>
+  )
+}
---
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/pages/Checkout/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+
+export const CheckoutContainer = styled.div``;
+
+export const Content = styled.div``;
