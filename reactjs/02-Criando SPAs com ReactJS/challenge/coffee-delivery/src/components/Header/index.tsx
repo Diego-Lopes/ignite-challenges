2af0e4fbc89ca054c3fd05c800c1cf9507ed0560
Diego-Lopes <diegolopes2009@hotmail.com>
2023-02-14 22:51:57 +0044

    chore: modification components

--- reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/components/Header/index.tsx
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/components/Header/index.tsx
@@ -3,19 +3,20 @@ import { MapPin, ShoppingCart } from "phosphor-react";
 import logo from "../../../src/assets/Logo.svg";
 import { useContext } from "react";
 import { StorageContext } from "../../context/StorageContext";
+import { NavLink } from "react-router-dom";
 export function Header() {
   const { countInTheCart } = useContext(StorageContext);
-  
+
   function handleScroll() {
-    const header= document.querySelector("header")as HTMLElement;
-    
+    const header = document.querySelector("header") as HTMLElement;
+
     if (window.pageYOffset > header.offsetTop) {
       header.classList.add("fixed-header");
     } else {
       header.classList.remove("fixed-header");
     }
   }
-  
+
   window.addEventListener("scroll", handleScroll);
   return (
     <S.HeaderContainer>
@@ -27,9 +28,11 @@ export function Header() {
             Ji-Paraná/RO
           </S.ButtonLocal>
           {countInTheCart > 0 ? (
-            <S.ButtonCheckout howManyInTheCart={countInTheCart}>
-              <ShoppingCart size={22} weight="fill" />
-            </S.ButtonCheckout>
+            <NavLink to="/checkout">
+              <S.ButtonCheckout howManyInTheCart={countInTheCart}>
+                <ShoppingCart size={22} weight="fill" />
+              </S.ButtonCheckout>
+            </NavLink>
           ) : (
             <S.ButtonCheckout>
               <ShoppingCart size={22} weight="fill" />
