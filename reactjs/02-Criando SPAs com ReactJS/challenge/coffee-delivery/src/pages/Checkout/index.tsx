863736c0a002adf357537088d102fa61e632e949
Diego-Lopes <diegolopes2009@hotmail.com>
2023-02-15 23:55:03 +0044

    chore: added new elements html

--- reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/pages/Checkout/index.tsx
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/pages/Checkout/index.tsx
@@ -1,12 +1,59 @@
-import * as S from './styles';
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
+import * as S from "./styles";
+import { StorageContext } from '../../context/StorageContext'
+import { useContext } from "react";
 
 export function Checkout() {
+  const { shoppingCart } = useContext(StorageContext);
+
+  console.log( shoppingCart);
+  
   return (
     <S.CheckoutContainer>
       <S.Content>
-        <S.Address></S.Address>
+        <S.Address>
+          <h2>Complete seu pedido</h2>
+          <S.Form>
+            <div className="address">
+              <MapPinLine size={32} />
+              <p>Endereço de entrega</p>
+              <p>informe o endereço onde deseja receber seu pedido</p>
+            </div>
+            <div className="boxInputs">
+              <input type="text" placeholder="CEP" />
+              <input type="text" placeholder="RUA" />
+              <input type="text" placeholder="NÚMERO" />
+              <input type="text" placeholder="COMPLEMENTO" />
+              <input type="text" placeholder="BAIRRO" />
+              <input type="text" placeholder="CIDADE" />
+              <input type="text" placeholder="UF" />
+            </div>
+            <div className="boxModePay">
+              <div className="boxInfo">
+                <div className="title">
+                  <CurrencyDollar size={32} />
+                  <h3>Pagamento</h3>
+                </div>
+                <div className="boxOptions">
+                  <button type="button">
+                    <CreditCard size={16}/>
+                    cartão de crédito
+                  </button>
+                  <button type="button">
+                    <Bank size={16} />
+                    cartão de débito
+                  </button>
+                  <button type="button">
+                    <Money size={16} />
+                    dinheiro
+                  </button>
+                </div>
+              </div>
+            </div>
+          </S.Form>
+        </S.Address>
         <S.ShoppingCart></S.ShoppingCart>
       </S.Content>
     </S.CheckoutContainer>
-  )
+  );
 }
--- reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/pages/Checkout/styles.ts
+++ reactjs/02-Criando SPAs com ReactJS/challenge/coffee-delivery/src/pages/Checkout/styles.ts
@@ -7,4 +7,6 @@ export const Content = styled.div``;
 
 export const Address = styled.div``;
 
+export const Form = styled.form``;
+
 export const ShoppingCart = styled.div``;
