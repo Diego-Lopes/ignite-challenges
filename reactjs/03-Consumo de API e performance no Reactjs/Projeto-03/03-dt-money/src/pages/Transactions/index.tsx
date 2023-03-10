b4ad6bba65765d095716dbbae6c71d066189c69a
Diego-Lopes <diegolopes2009@hotmail.com>
2023-03-10 23:43:27 +0044

    feat: added component and style of table

--- reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/pages/Transactions/index.tsx
+++ reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
-import { Header } from '../../components/Header'
-import { Summary } from '../../components/Summary'
-import * as S from './styles'
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import * as S from "./styles";
 export function Transaction() {
   return (
     <S.ContainerTransaction>
       <Header />
       <Summary />
+      <S.TransactionContainers>
+        <S.TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <S.PriceHighlight variant="income">
+                  R$ 12.000,00
+                </S.PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Despesa com servidor</td>
+              <td>
+                <S.PriceHighlight variant="outcome">- R$ 59,00</S.PriceHighlight>
+              </td>
+              <td>Infra</td>
+              <td>10/03/2022</td>
+            </tr>
+          </tbody>
+        </S.TransactionTable>
+      </S.TransactionContainers>
     </S.ContainerTransaction>
-  )
+  );
 }
