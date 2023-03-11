2173a5fedeea2aa612e1e1cd8497262611229ad4
Diego-Lopes <diegolopes2009@hotmail.com>
2023-03-11 16:15:58 +0044

    feat: added new lib Radix-ui Dialog

--- reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/components/Header/index.tsx
+++ reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/components/Header/index.tsx
@@ -1,12 +1,28 @@
 import * as S from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImg from "../../assets/logo.svg";
 
+/**
+ * esse asChild ele faz com que o Dialog.trigger não crie um novo botton e
+ * sim incorpora o botton que está dentro dele no caso o NewTransactionButton.
+ */
 export function Header() {
   return (
     <S.ContainerHeader>
       <S.HeaderContent>
         <img src={logoImg} alt="" />
-        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay/>
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close/>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </S.HeaderContent>
     </S.ContainerHeader>
   );
