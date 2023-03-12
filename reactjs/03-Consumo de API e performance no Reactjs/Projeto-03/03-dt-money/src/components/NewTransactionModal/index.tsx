746a8a4b0df32647b10702b967ec95bf4802409d
diego-lopes <diegolopes2009@hotmail.com>
2023-03-11 22:56:53 -0300

    chore: modification component modal

--- reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 import * as S from "./style";
 export function NewTransactionModal() {
   return (
@@ -15,6 +15,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <S.TransactionType>
+            <S.TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </S.TransactionTypeButton>
+            <S.TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </S.TransactionTypeButton>
+          </S.TransactionType>
+
           <button type="submit">Cadastrar</button>
         </form>
       </S.Content>
