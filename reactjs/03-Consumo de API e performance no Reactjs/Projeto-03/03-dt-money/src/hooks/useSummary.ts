53652b0561208a42e56af70f57a2af83bce208c2
Diego-Lopes <diegolopes2009@hotmail.com>
2023-03-19 06:27:08 +0044

    chore: added useMemo

--- reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/hooks/useSummary.ts
+++ reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/hooks/useSummary.ts
@@ -1,23 +1,36 @@
+import { useMemo } from 'react'
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactiosContext'
+
+/**
+ * comparando as variáveis verificando se mudou o conteúdo se mudou renderizar
+ * usando useMemo
+ *
+ * useMemo recebe uma função e array de dempedencias.
+ *
+ * Um hook focado em performance que memoriza valores computados
+ * e reavalia esses valores caso uma de suas dependências seja alterada.
+ */
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions
   })
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc
-    },
-    { income: 0, outcome: 0, total: 0 },
-  )
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+        return acc
+      },
+      { income: 0, outcome: 0, total: 0 },
+    )
+  }, [transactions])
 
   return summary
 }
