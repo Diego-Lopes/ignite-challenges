eadf4287de9dc577a9e1c8fe3c776f7c584f1316
Diego-Lopes <diegolopes2009@hotmail.com>
2023-03-19 06:14:37 +0044

    chore: explaining use of memo

--- reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/components/SearchForm/index.tsx
+++ reactjs/03-Consumo de API e performance no Reactjs/Projeto-03/03-dt-money/src/components/SearchForm/index.tsx
@@ -5,6 +5,26 @@ import * as z from 'zod'
 import * as S from './styles'
 import { TransactionsContext } from '../../contexts/TransactiosContext'
 import { useContextSelector } from 'use-context-selector'
+/**
+ * import { memo } from 'react'
+ *
+ * Por que que um componente renderiza?
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed (mudou propriedades);
+ * - Parent rerendered (componente pai renderizou);
+ *
+ * Qual o fluxo de renderização?
+ * 1. O React recria o HTML da interface daquele componente
+ * 2. Compara a versão do HTML recriada com a versão anterior
+ * 3. Se mudou alguma coisa, ele reescreve o HTML na tela
+ *
+ * Memo:
+ * passos do memo:
+ * 0. Hooks changed, Props changed (deep comparison) //fazendo comparação profunda em hooks, props
+ * 0.1: Comparar a versão anterior dos hooks e props
+ * 0.2: Se mudou algo, ele vai permitir a nova renderização
+ *
+ */
 
 const searchFormSchema = z.object({
   query: z.string(),
