fd6a4e486698455a5ef4f7378fe81db1bfde1976
Diego-Lopes <diegolopes2009@hotmail.com>
2023-03-24 00:10:53 +0044

    feat: started challenge github blog

---
+++ reactjs/03-Consumo de API e performance no Reactjs/challenge/github_blog/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styled'
+
+export function Home() {
+  return (
+    <S.HomeContainer>
+      <S.HomeContent>
+        <h1>Hello World!</h1>
+      </S.HomeContent>
+    </S.HomeContainer>
+  )
+}
---
+++ reactjs/03-Consumo de API e performance no Reactjs/challenge/github_blog/src/pages/home/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  background: ${(props) => props.theme['gray-900']};
+  width: 100%;
+  height: 100%;
+`
+
+export const HomeContent = styled.div`
+  width: 100%;
+  max-width: 136rem;
+`
