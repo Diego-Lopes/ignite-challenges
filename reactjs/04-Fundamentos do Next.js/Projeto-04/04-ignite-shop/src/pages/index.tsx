b6a9df106fb66a7b64fc6cbece4508aaf3cb5fed
Diego-Lopes <diegolopes2009@hotmail.com>
2023-04-01 17:33:14 +0044

    chore: added new feate card product

--- reactjs/04-Fundamentos do Next.js/Projeto-04/04-ignite-shop/src/pages/index.tsx
+++ reactjs/04-Fundamentos do Next.js/Projeto-04/04-ignite-shop/src/pages/index.tsx
@@ -1,14 +1,35 @@
 import { styled } from '@/styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
+
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
+import camiseta4 from '../assets/camisetas/4.png';
 
 const inter = Inter({ subsets: ['latin'] })
 const Button = styled("button", {
   backgroundColor: '$green300'
 })
+
 export default function Home() {
   return (
-    <>
-      <Button>Enviar</Button>
-    </>
+      <HomeContainer>
+        <Product>
+          <Image src={camiseta1} width={520} height={480} alt={''} />
+          <footer>
+            <strong>Camisate X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+        <Product>
+          <Image src={camiseta2} width={520} height={480} alt={''} />
+          <footer>
+            <strong>Camisate X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
   )
 }
