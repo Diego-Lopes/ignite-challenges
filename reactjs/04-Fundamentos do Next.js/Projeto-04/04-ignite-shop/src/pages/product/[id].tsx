62d4fec35557727305d8e283bafacec4195ebcae
diego-lopes <diegolopes2009@hotmail.com>
2023-04-06 16:11:47 -0300

    chore: modification modules page product

--- reactjs/04-Fundamentos do Next.js/Projeto-04/04-ignite-shop/src/pages/product/[id].tsx
+++ reactjs/04-Fundamentos do Next.js/Projeto-04/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,29 +1,62 @@
 import * as S from "@/styles/pages/product";
-import { useRouter } from "next/router";
 
 import camisa1 from "../../assets/camisetas/1.png";
 import Image from "next/image";
-export default function ProductId() {
-  const { query } = useRouter();
+import { GetStaticProps } from "next";
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
+
+interface ProductIdProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function ProductId({ product }: ProductIdProps) {
   return (
     <S.ProductContainer>
       <S.ImageContainer>
-        <Image src={camisa1} alt={""} quality={100} />
+        <Image src={product.imageUrl} alt={""} quality={100} width={520} height={480}/>
       </S.ImageContainer>
       <S.ProductDatails>
-        <h1>Camiseta Y</h1>
+        <h1>{product?.name}</h1>
+        <span>{product?.price}</span>
+        <p>{product?.description}</p>
 
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque sit
-          placeat labore sapiente explicabo saepe, architecto blanditiis
-          exercitationem amet vero quos corporis et voluptatum fugiat, in odit,
-          adipisci molestiae doloremque!
-        </p>
-
-        <button>
-          Comprar agora
-        </button>
+        <button>Comprar agora</button>
       </S.ProductDatails>
     </S.ProductContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = String(params?.id);
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount! / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1h
+  };
+};
