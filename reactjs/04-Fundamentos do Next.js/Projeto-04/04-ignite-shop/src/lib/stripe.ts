f3a80e1e6cd16cc47f5e50b68e0326dc30e77c3e
Diego-Lopes <diegolopes2009@hotmail.com>
2023-04-23 15:32:28 -0300

    chore: finished ✅

--- reactjs/04-Fundamentos do Next.js/Projeto-04/04-ignite-shop/src/lib/stripe.ts
+++ reactjs/04-Fundamentos do Next.js/Projeto-04/04-ignite-shop/src/lib/stripe.ts
@@ -1,12 +1,9 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(
-  "sk_test_51MsejQItRknFcmnlmhk1KQfCyiTUY7ylv2ejHc398ZiuLvLxgDG5iGIaskd7bOf8xCH4hrRXRyb0dRiAwA3GOocL008rXy4LaE",
-  {
-    apiVersion: "2022-11-15",
-    appInfo: {
-      //fica o log das requisições no stripe.
-      name: "ignite shop",
-    },
-  }
-);
+export const stripe = new Stripe(process.env.STRIPE_SECRETE_KEY as string, {
+  apiVersion: "2022-11-15",
+  appInfo: {
+    //fica o log das requisições no stripe.
+    name: "ignite shop",
+  },
+});
