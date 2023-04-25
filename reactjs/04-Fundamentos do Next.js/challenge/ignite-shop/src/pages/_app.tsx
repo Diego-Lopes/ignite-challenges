dfb2d875fabf697c668b1f4a3466b9f91549da4e
Diego Lopes <diegolopes2009@hotmail.com>
2023-04-25 00:14:40 -0300

    chore: added compoenent context shoppingCartProvider

--- reactjs/04-Fundamentos do Next.js/challenge/ignite-shop/src/pages/_app.tsx
+++ reactjs/04-Fundamentos do Next.js/challenge/ignite-shop/src/pages/_app.tsx
@@ -3,15 +3,18 @@ import { Container, Header } from "@/styles/pages/app";
 import type { AppProps } from "next/app";
 import Image from "next/image";
 import logoImg from "../assets/logo.svg";
+import { ShoppingcartLocalStorageProvider } from "@/context/shoppingcart";
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt=""/>
+        <Image src={logoImg} alt="" />
       </Header>
-      <Component {...pageProps} />
+      <ShoppingcartLocalStorageProvider>
+        <Component {...pageProps} />
+      </ShoppingcartLocalStorageProvider>
     </Container>
   );
 }
