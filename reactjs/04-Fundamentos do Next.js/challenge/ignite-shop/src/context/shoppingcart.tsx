ddee148ecbbb5ef68635f00d37417533d68284a6
Diego Lopes <diegolopes2009@hotmail.com>
2023-04-25 00:14:06 -0300

    feat: created context shoppingCart

--- reactjs/04-Fundamentos do Next.js/challenge/ignite-shop/src/context/shoppingcart.tsx
+++ reactjs/04-Fundamentos do Next.js/challenge/ignite-shop/src/context/shoppingcart.tsx
@@ -1,1 +1,40 @@
 // this is shoppingcart
+
+import { ReactNode, createContext, useState } from "react";
+
+interface DataProps {
+  id: string;
+  imgUrl: string;
+  title: string;
+  price: string;
+}
+
+interface LocalStorageShoppingCartContextProps {
+  data: DataProps[];
+  countInTheShoppcart: number;
+}
+
+export const LocalStorageShoppcartContext = createContext(
+  {} as LocalStorageShoppingCartContextProps
+);
+
+interface ShoppingCartLocalStorageProviderProps {
+  children: ReactNode;
+}
+
+export function ShoppingcartLocalStorageProvider({
+  children,
+}: ShoppingCartLocalStorageProviderProps) {
+  const [data, setData] = useState<DataProps[]>([]);
+  const [countInTheShoppcart, setCountInTheShoppcart] = useState(0);
+
+  //add in shoppcart
+
+  return (
+    <LocalStorageShoppcartContext.Provider
+      value={{ data, countInTheShoppcart }}
+    >
+      {children}
+    </LocalStorageShoppcartContext.Provider>
+  );
+}
