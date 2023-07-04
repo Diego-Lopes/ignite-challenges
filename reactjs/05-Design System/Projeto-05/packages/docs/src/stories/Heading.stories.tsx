6229a95b339a87efeb4f1e892cffd7624d1320bc
diego-lopes <diegolopes2009@hotmail.com>
2023-07-04 17:27:45 -0400

    feat: 🆕created new component

---
+++ reactjs/05-Design System/Projeto-05/packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,21 @@
+import { Heading, HeadingProps } from '@ignite-ui-diego/react'
+import type { StoryObj, Meta } from '@storybook/react' // typagem do typescript
+
+export default {
+  // essa é a configuração global
+  title: 'Typography/Heading',
+  component: Heading,
+
+  // args são propriedades
+  args: {
+    children: 'Custom Title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+}
