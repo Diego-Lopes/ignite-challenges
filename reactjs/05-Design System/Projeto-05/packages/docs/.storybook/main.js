c50bf347aaa10970a03fac16e4b31e2679eeb197
diego-lopes <diegolopes2009@hotmail.com>
2023-07-04 10:43:47 -0400

    chore: 🔄️added addons-doc in array of addons

--- reactjs/05-Design System/Projeto-05/packages/docs/.storybook/main.js
+++ reactjs/05-Design System/Projeto-05/packages/docs/.storybook/main.js
@@ -1,13 +1,15 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: [
-    "../src/stories/**/*.mdx",
+    "../src/pages/**/*.stories.mdx",
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+
   ],
   framework: {
     name: "@storybook/react-vite",
