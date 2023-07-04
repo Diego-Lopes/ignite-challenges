78ac0eb65ce7a5ec38e37f959732e2eb423c6191
diego-lopes <diegolopes2009@hotmail.com>
2023-07-04 10:07:26 -0400

    refactore: ♻️components storybook

--- reactjs/05-Design System/Projeto-05/packages/docs/vite.config.js
+++ reactjs/05-Design System/Projeto-05/packages/docs/vite.config.js
@@ -1,7 +1,7 @@
-///<reference types="vite/client" />
-//faz com que typescript reconhece algumas tipagens globais do vite
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+/// <reference types= "vite/client" />
+// faz com que typescript reconhece algumas tipagens globais do vite
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
-});
+})
--- reactjs/05-Design System/Projeto-05/packages/react/src/index.tsx
+++ reactjs/05-Design System/Projeto-05/packages/react/src/index.tsx
@@ -1,12 +1,30 @@
 import { styled } from './styles'
+import { ComponentProps } from 'react'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
-  width: '$10',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello World</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
