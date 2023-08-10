cb1170eda56536fda24b9cffdea578bcfff7b47b
diego-lopes <diegolopes2009@hotmail.com>
2023-08-10 14:15:58 -0400

    ✨feat: added authentication social with next-auth

--- reactjs/06-Ignite Call/Projeto-06/ignite-call/src/pages/register/connect-calendar/index.page.tsx
+++ reactjs/06-Ignite Call/Projeto-06/ignite-call/src/pages/register/connect-calendar/index.page.tsx
@@ -3,6 +3,7 @@ import { Container, Header } from '../styles'
 import { ArrowRight } from 'phosphor-react'
 // import { api } from '@/lib/axios'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn } from 'next-auth/react'
 
 export default function Register() {
   // async function handleRegister(data: RegisterFormData) {}
@@ -21,7 +22,11 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight weight="bold" />
           </Button>
