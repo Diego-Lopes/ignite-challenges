d8c8959480845cd0c308f15d5fb5c3edaec5f8dc
Diego-Lopes <diegolopes2009@hotmail.com>
2023-08-09 22:25:43 -0400

    ♻️refactore: added error handling username exists

--- reactjs/06-Ignite Call/Projeto-06/ignite-call/src/pages/api/users/index.api.ts
+++ reactjs/06-Ignite Call/Projeto-06/ignite-call/src/pages/api/users/index.api.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { setCookie } from 'nookies'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
@@ -11,11 +12,31 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  // verificar no banco se já existe o usuário
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  // se retornar algo do banco, retorne um code error
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already exists.',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  // armazenando dados no cookie
+  setCookie({ res }, '@ignitecall:userID', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 dias
+    path: '/',
   })
 
   return res.status(201).json(user)
