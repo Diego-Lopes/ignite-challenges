3aea7e5721295c70d08e59ac5c17a96f5843013a
diego-lopes <diegolopes2009@hotmail.com>
2023-08-14 13:57:58 -0400

    ✨feat: create function get week days

---
+++ reactjs/06-Ignite Call/Projeto-06/ignite-call/src/utils/get-week-days.ts
@@ -0,0 +1,15 @@
+export function getWeekDays() {
+  const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
+
+  /**
+   * .from() função faz converção para array conteúdo interável
+   * Array() cria um array recebendo o número de posições
+   * .keys() faz a com que o retorno das posições do array que estamos criando
+   * retorno nos campos undefined em index da possição no array
+   */
+  return Array.from(Array(7).keys())
+    .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
+    .map((weekDay) => {
+      return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+    })
+}
