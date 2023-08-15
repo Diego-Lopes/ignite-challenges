487a868053a4e4691473f14066d0630ae5bb8018
diego-lopes <diegolopes2009@hotmail.com>
2023-08-15 17:10:12 -0400

    ✨feat: created page update profile

---
+++ reactjs/06-Ignite Call/Projeto-06/ignite-call/src/pages/register/update-profile/index.page.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Button, Heading, MultiStep, Text, TextArea } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Container, Header } from '../styles'
+import { FormAnnotation, ProfileBox } from './styles'
+import { useSession } from 'next-auth/react'
+import { GetServerSideProps } from 'next'
+import { getServerSession } from 'next-auth'
+import { buildNextAuthOptions } from '@/pages/api/auth/[...nextauth].api'
+
+const updateProfileSchema = z.object({
+  bio: z.string(),
+})
+
+type UpdateProfileData = z.infer<typeof updateProfileSchema>
+
+export default function UpdateProfile() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<UpdateProfileData>({
+    resolver: zodResolver(updateProfileSchema),
+  })
+
+  // pegando sessão salva, tais como imagem e nome.
+  const session = useSession()
+  console.log(session)
+
+  async function handleUpdateProfile(data: UpdateProfileData) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essa informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
+        <label>
+          <Text>Foto de perfil</Text>
+        </label>
+        <label>
+          <Text size="sm">Sobre você</Text>
+          <TextArea {...register('bio')} />
+          <FormAnnotation size="sm">
+            Fale um pouco sobre você. Isto será exibido em sua página pessoal.
+          </FormAnnotation>
+        </label>
+
+        <Button type="submit" disabled={isSubmitting}>
+          Finalizar
+          <ArrowRight weight="bold" />
+        </Button>
+      </ProfileBox>
+    </Container>
+  )
+}
+
+// pegando os cookies através do server side e implementando em props
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  // pegando a session de getServerSession
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  return {
+    props: {
+      session,
+    },
+  }
+}
