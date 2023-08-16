9b048b86c8853296c66d4e7213ab7ec2e4720585
diego-lopes <diegolopes2009@hotmail.com>
2023-08-16 08:43:36 -0400

    🧱ci: added register domains images

--- reactjs/06-Ignite Call/Projeto-06/ignite-call/next.config.js
+++ reactjs/06-Ignite Call/Projeto-06/ignite-call/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  images: {
+    domains: ["images.prismic.io", "localhost", "lh3.googleusercontent.com"],
+  },
 }
 
 module.exports = nextConfig
