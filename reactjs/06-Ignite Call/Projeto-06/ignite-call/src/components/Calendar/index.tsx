adb3775dc098c239ad635205f00a5434e3f2c7a9
Diego-Lopes <diegolopes2009@hotmail.com>
2023-08-19 23:02:18 -0400

    ♻️refactore: added function blockedDay

--- reactjs/06-Ignite Call/Projeto-06/ignite-call/src/components/Calendar/index.tsx
+++ reactjs/06-Ignite Call/Projeto-06/ignite-call/src/components/Calendar/index.tsx
@@ -32,6 +32,7 @@ interface CalendarProps {
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
@@ -66,7 +67,7 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: currentDate.get('month') + 1,
         },
       })
       return response.data
@@ -78,6 +79,9 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
    */
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
     // 1 quantos dias existe no mês?
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
@@ -137,7 +141,8 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
