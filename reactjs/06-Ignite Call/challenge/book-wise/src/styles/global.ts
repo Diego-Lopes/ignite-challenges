b0d93567807cced3fbb7b6576b7c6fd71a278b2c
Diego-Lopes <diegolopes2009@hotmail.com>
2023-08-28 15:49:12 -0400

    ✨feat: started challenge 06

---
+++ reactjs/06-Ignite Call/challenge/book-wise/src/styles/global.ts
@@ -0,0 +1,13 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
+
+  body: {
+    "-webkit-font-smoothing": "antialiased",
+  }
+})
