fefac6d6ae9618aba081f9621a21db121c985daf
Diego-Lopes <diegolopes2009@hotmail.com>
2023-08-31 16:47:06 -0400

    ♻️refactore: added data persistence

--- nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/database.js
+++ nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/database.js
@@ -1,8 +1,34 @@
+import fs from 'node:fs/promises'
 
+const databasePath = new URL('../db.json', import.meta.url)
+
+console.log(databasePath.pathname);
 
 export class Database {
   //para privar a variável usa # 
   #database = {}
+
+  constructor() {
+    //método construtor que é criado quando é executado.
+    /**
+     * essa função fs.readFile faz uma leitura do arquivo db.json
+     * verificando se existe conteúdo, caso não exista conteúdo ou o arquivo em si
+     * no catch ele criar o arquivo db.json vazio.
+     */
+    fs.readFile(databasePath, 'utf8').then((data) => {
+      //se existe algo em data ele atribui na variável privada #database
+      this.#database = JSON.parse(data)
+    })
+    .catch(() => {
+      this.#persist()
+    })
+  }
+
+
+  #persist() {
+    //fs.writeFile recebe arquivo json
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   //método select para fazer seleção
   /**
@@ -33,6 +59,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist();
 
     return data //retornar o item que foi inserido.
   }
