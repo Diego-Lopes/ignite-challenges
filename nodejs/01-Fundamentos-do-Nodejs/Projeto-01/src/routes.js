1c548769aea883615f2485671ff57ccaa486318c
Diego-Lopes <diegolopes2009@hotmail.com>
2023-08-31 17:09:10 -0400

    ✨feat: created file router

---
+++ nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/routes.js
@@ -0,0 +1,35 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from "./database.js"
+
+//persistindo dados.
+const database = new Database()
+
+export const routes = [
+  {
+    method: "GET",
+    path: "/users",
+    handler: (req, res) => {
+      const users = database.select("users");
+
+      return res.end(JSON.stringify(users));
+    },
+  },
+  {
+    method: "POST",
+    path: "/users",
+    handler: (req, res) => {
+      const { name, email } = req.body;
+
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      };
+
+      //salvando na tabela.
+      database.insert("users", user);
+
+      return res.writeHead(201).end();
+    },
+  },
+];
