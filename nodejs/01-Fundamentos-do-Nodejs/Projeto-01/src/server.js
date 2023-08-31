9e2f81cbc47818815ddb792a702dac38f76e9d4d
Diego-Lopes <diegolopes2009@hotmail.com>
2023-08-31 17:09:44 -0400

    ♻️refactore: modification router path in server

--- nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/server.js
+++ nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/server.js
@@ -1,6 +1,6 @@
 import http from "node:http";
 import { json } from "./middleware/json.js";
-import { Database } from "./database.js";
+import { routes } from "./routes.js";
 
 // - Criar usuários
 // - Listagem usuários
@@ -26,37 +26,24 @@ import { Database } from "./database.js";
 
 //Cabeçalhos (Requisição/resposta) => Metadados (informações para auxiliar os dados enviados da requisição)
 
-//persistindo dados.
-const database = new Database()
+
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
   
   await json(req, res)
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select('users')
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-    return res
-      .end(JSON.stringify(users))
+  console.log(route);
+
+  if (route) {
+    return route.handler(req, res)
   }
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body
-
-   const user = {
-      id: 1,
-      name,
-      email,
-    }
-
-    //salvando na tabela. 
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
-  }
-
-  return res.end("Hello world")
+  return res.writeHead(404).end()
 });
 
 server.listen(3333)
