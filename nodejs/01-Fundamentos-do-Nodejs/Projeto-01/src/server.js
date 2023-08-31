713368cf1bae38669f9f03f9410a00f4a137aaca
Diego-Lopes <diegolopes2009@hotmail.com>
2023-08-31 11:04:19 -0400

    ♻️refactored: modification in server

--- nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/server.js
+++ nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { json } from "./middleware/json.js";
 
 // - Criar usuários
 // - Listagem usuários
@@ -26,25 +27,28 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
-  const { method, url } = req;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+  
+  await json(req, res)
 
   if (method === "GET" && url === "/users") {
     return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+      .end(JSON.stringify(users))
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name,
+      email,
     });
-    return res.end("Criação de usuário");
+    return res.writeHead(201).end()
   }
 
-  return res.end("Hello world");
+  return res.end("Hello world")
 });
 
-server.listen(3333);
+server.listen(3333)
