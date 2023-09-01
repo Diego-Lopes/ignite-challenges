b037066ff6bb9f22deb99f6401d7be4548860f4e
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-01 10:43:56 -0400

    ♻️refactore: added pathRegex

--- nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/server.js
+++ nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/server.js
@@ -34,12 +34,16 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   console.log(route);
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+
+    console.log(routeParams)
+
     return route.handler(req, res)
   }
 
