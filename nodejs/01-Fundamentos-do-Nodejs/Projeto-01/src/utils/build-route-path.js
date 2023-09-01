1881f797623b253c06dc309ae32324b365d0d266
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-01 10:43:21 -0400

    ♻️refactore: added pathRegex

--- nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/utils/build-route-path.js
+++ nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/utils/build-route-path.js
@@ -3,5 +3,13 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]*)/g
   
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+  
   console.log(Array.from(path.matchAll(routeParametersRegex)))
+  // [ ':id', 'id', index: 7, input: '/users/:id', groups: undefined ]
+  console.log(pathWithParams);
+  // /users/([a-z0-9-_]+)
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+
+  return pathRegex
 }
