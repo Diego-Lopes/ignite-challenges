69bdb972709824f43cf727697d8f3848862daa32
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-01 10:07:27 -0400

    ♻️refactore: added function in paths buildRoutePath

--- nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/routes.js
+++ nodejs/01-Fundamentos-do-Nodejs/Projeto-01/src/routes.js
@@ -1,13 +1,14 @@
-import { randomUUID } from 'node:crypto'
-import { Database } from "./database.js"
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 //persistindo dados.
-const database = new Database()
+const database = new Database();
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const users = database.select("users");
 
@@ -16,7 +17,7 @@ export const routes = [
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -30,6 +31,13 @@ export const routes = [
       database.insert("users", user);
 
       return res.writeHead(201).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      return res.end();
     },
   },
 ];
