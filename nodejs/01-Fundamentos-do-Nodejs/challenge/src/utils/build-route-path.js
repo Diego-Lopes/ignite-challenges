aeb38f10825cec7bb03ed8656c597c1fceb562a9
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-13 21:36:41 -0400

    ✨feat: created new component build router path

---
+++ nodejs/01-Fundamentos-do-Nodejs/challenge/src/utils/build-route-path.js
@@ -0,0 +1,19 @@
+// estração de variável :id
+
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]*)/g
+
+  const pathWithParams = path.replaceAll(
+    routeParametersRegex,
+    "(?<$1>[a-z0-9-_]+)"
+  );
+
+  console.log(Array.from(path.matchAll(routeParametersRegex)));
+  // [ ':id', 'id', index: 7, input: '/users/:id', groups: undefined ]
+  console.log(pathWithParams);
+  // /users/([a-z0-9-_]+)
+  // const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$)`)
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+
+  return pathRegex
+}
