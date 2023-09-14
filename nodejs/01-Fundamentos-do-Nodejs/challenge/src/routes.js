4150e48a20705d184f16f2c325e2f8ec667cfe64
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-13 21:35:29 -0400

    ✨feat: created server in path src

---
+++ nodejs/01-Fundamentos-do-Nodejs/challenge/src/routes.js
@@ -0,0 +1,54 @@
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = []
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+     handler: (req, res) => {
+      console.log(req.query);
+      return res.writeHead(200).end(JSON.stringify(database))
+    },
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+
+      const { title, description } = req.body
+      console.log({title, description});
+      const ObjTask = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        update_at: null,
+      }
+
+      database.push(ObjTask);
+
+      console.log(database);
+
+
+      return res.writeHead(201).end()
+    },
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+
+      const { id } = req.params
+      const { title, description } = req.body
+      console.log({title, description, id});
+
+      
+     
+      return res.writeHead(204).end()
+    },
+  },
+
+];
