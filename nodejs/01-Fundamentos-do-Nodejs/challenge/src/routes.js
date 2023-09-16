8ff1ac3880f74309d88d35d2ed4ecc4c4dec618b
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-16 18:58:38 -0400

    ✨feat: created route patch

--- nodejs/01-Fundamentos-do-Nodejs/challenge/src/routes.js
+++ nodejs/01-Fundamentos-do-Nodejs/challenge/src/routes.js
@@ -46,6 +46,8 @@ export const routes = [
       if (findIndex > -1) {
         database.splice(findIndex, 1);
         return res.writeHead(204).end()
+      } else {
+        return res.writeHead(404).end()
       }
     },
   },
@@ -55,7 +57,6 @@ export const routes = [
     handler: (req, res) => {
       const { id } = req.params;
       const { title, description } = req.body;
-      // console.log({ title, description, id });
 
       database.filter((filter) => {
         if (filter.id === id) {
@@ -71,6 +72,37 @@ export const routes = [
             });
 
           return res.writeHead(204).end();
+        } else {
+          return res.writeHead(404).end()
+        }
+      });
+    },
+  },
+  {
+    method: "PATCH",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.filter((filter) => {
+        if (filter.id === id) {
+          database
+            .filter((filter) => {
+              return filter.id === id;
+            })
+            .map((obj) => {
+              if(obj.completed_at === null) {
+                obj.completed_at = true
+                obj.update_at = new Date();
+              } else if(obj.completed_at === true) {
+                obj.completed_at = null
+                obj.update_at = new Date();
+              } 
+            });
+
+          return res.writeHead(204).end();
+        } else {
+          return res.writeHead(404).end()
         }
       });
     },
