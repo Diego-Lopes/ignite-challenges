26f024737f76f148f7ced2c1a91aa9a023eeb0c7
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-16 22:19:45 -0400

    ✨feat: created project 02 API-REST

---
+++ nodejs/02-Criando-API-REST-com-Nodejs/src/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello world'
+})
+
+app
+  .listen({ port: 3333 })
+  .then(() => console.log('HTTP Server listening on port 3333'))
