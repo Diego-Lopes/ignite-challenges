a869462d79f461a6b3e1e87d43bbf22bfb8813ba
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-18 17:23:35 -0400

    ♻️refactore: added comments

--- nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/database.ts
+++ nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/database.ts
@@ -1,9 +1,12 @@
 import { knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite', // obrigatório
   connection: {
     // obrigatório
     filename: './tmp/app.db',
   },
-})
+  useNullAsDefault: true, // faz inserção nos campos das tabelas conteúno nulos por padrão
+}
+
+export const knex = setupKnex(config)
