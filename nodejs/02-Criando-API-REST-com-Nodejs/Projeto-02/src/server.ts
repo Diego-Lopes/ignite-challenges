6c7e746fcb23b7a63863a6cd68858c8220b86cf1
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-20 00:36:23 -0400

    ♻️refactore: create query

--- nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/server.ts
+++ nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/server.ts
@@ -1,11 +1,23 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  // const transaction = knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  // return transaction
+
+  // traz todas as transações do banco de dados
+  const transactions = await knex('transactions').select('*')
+  return transactions
 })
 
 app
