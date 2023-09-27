a30e005311cf93abe0d814817998e055e146136b
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-26 20:11:58 -0400

    ✨feat: created file test

--- nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/test/example.spec.ts
+++ nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/test/example.spec.ts
@@ -1,11 +1,43 @@
 // fazendo o 1 teste
 
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('O usuário consegue criar uma nova transação', () => {
-  // fazer a chamada http para criar uma nova transação
+// criando categoria
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    // executar antes de tudo
+    await app.ready()
+    // ready vai aguardar todas as promisse sejam resolvidas
+  })
 
-  const responseStatusCode = 201
+  afterAll(async () => {
+    // depois de tudo
+    await app.close()
+    // close descarta a aplicação de teste.
+  })
 
-  expect(responseStatusCode).toEqual(201)
+  /**
+   * pode usar test ou it
+   * it é para deixa semântico com a frase.
+   * it('should be able to ...', () => {})
+   * significa: deve ser possível fazer...
+   */
+
+  it('should be able to create a new transactions', async () => {
+    // fazer a chamada http para criar uma nova transação
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+
+  })
 })
