6e437489dcc0486f1a8f26aecf271c59501d7473
Diego-Lopes <diegolopes2009@hotmail.com>
2023-09-27 23:14:13 -0400

    ♻️refactore: modification config dotenv

--- nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/env/index.ts
+++ nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/env/index.ts
@@ -1,5 +1,14 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
+
 // fazendo tratativa de erro com zod
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
