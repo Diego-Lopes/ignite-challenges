a82af36ef5c13b015569455a373be7fd9f88ac4d
Diego-Lopes <diegolopes2009@hotmail.com>
2023-10-01 12:58:41 -0400

    ♻️refactore: modification config of knex.Config

--- nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/database.ts
+++ nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/database.ts
@@ -2,11 +2,13 @@ import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite', // obrigatório
-  connection: {
-    // obrigatório
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT, // obrigatório
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+        filename: env.DATABASE_URL,
+      }
+      : env.DATABASE_URL,
   useNullAsDefault: true, // faz inserção nos campos das tabelas conteúdo nulos por padrão
   migrations: {
     extension: 'ts',
