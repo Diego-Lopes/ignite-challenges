b97737d11efc214e4aaa24a0b9b02f6a4b577a49
Diego-Lopes <diegolopes2009@hotmail.com>
2023-10-05 22:34:01 -0400

    🐛fix: fixed bug in database config

--- nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/database.ts
+++ nodejs/02-Criando-API-REST-com-Nodejs/Projeto-02/src/database.ts
@@ -5,9 +5,7 @@ export const config: Knex.Config = {
   client: env.DATABASE_CLIENT, // obrigatório
   connection:
     env.DATABASE_CLIENT === 'sqlite'
-      ? {
-        filename: env.DATABASE_URL,
-      }
+      ? { filename: env.DATABASE_URL }
       : env.DATABASE_URL,
   useNullAsDefault: true, // faz inserção nos campos das tabelas conteúdo nulos por padrão
   migrations: {
