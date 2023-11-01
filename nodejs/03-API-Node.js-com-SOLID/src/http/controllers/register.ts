792d7be09710202f33eef8600ef74f25ca6d7b79
Diego-Lopes <diegolopes2009@hotmail.com>
2023-10-31 21:56:17 -0400

    ♻️`refactore`: refactore component register

--- nodejs/03-API-Node.js-com-SOLID/src/http/controllers/register.ts
+++ nodejs/03-API-Node.js-com-SOLID/src/http/controllers/register.ts
@@ -1,6 +1,6 @@
-import { prisma } from '@/lib/prisma'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { registerUseCase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,13 +11,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  await prisma.user.create({
-    data: {
+  try {
+    await registerUseCase({
       name,
       email,
-      password_hash: password,
-    },
-  })
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send()
+  }
 
   return reply.status(201).send()
 }
