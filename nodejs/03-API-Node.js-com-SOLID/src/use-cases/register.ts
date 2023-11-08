ad04a86613aeca4d5e6fc4b3995f310e687d78ba
Diego-Lopes <diegolopes2009@hotmail.com>
2023-11-07 20:13:01 -0400

    ♻️`refactore`: modification name of interface

--- nodejs/03-API-Node.js-com-SOLID/src/use-cases/register.ts
+++ nodejs/03-API-Node.js-com-SOLID/src/use-cases/register.ts
@@ -1,11 +1,16 @@
-import { UserRepository } from '@/repositoreis/users-repository'
+import { UsersRepository } from '@/repositoreis/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterUserCaseRequest {
   name: string
   email: string
   password: string
+}
+
+interface RegisterUserCaseResponse {
+  user: User
 }
 
 // Implementation SOLID
@@ -14,9 +19,13 @@ interface RegisterUserCaseRequest {
 export class RegisterUseCase {
   // o construtor onde podemos inserir as dependencias.
   // eslint-disable-next-line prettier/prettier
-  constructor(private usersRepository: UserRepository) { }
+  constructor(private usersRepository: UsersRepository) { }
 
-  async execute({ name, email, password }: RegisterUserCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUserCaseRequest): Promise<RegisterUserCaseResponse> {
     const password_hash = await hash(password, 6)
     /**
      * hash("", round)
@@ -34,10 +43,14 @@ export class RegisterUseCase {
     }
 
     // created user
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
