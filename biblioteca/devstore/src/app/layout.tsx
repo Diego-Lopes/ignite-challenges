53d3138ab64a3dd338ed11a631c59e78e7fbdbc5
Diego-Lopes <diegolopes2009@hotmail.com>
2023-11-17 23:50:02 -0400

    🎉`feat`: initial project

---
+++ biblioteca/devstore/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+/**
+ * Definindo a fonte em uma variável global chamamos de --font-inter,
+ * assim possibilita no arquivo tailwind.config.ts definirmos font famili sans:
+ * com nossa variavel --font-inter.
+ */
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+
+export const metadata: Metadata = {
+  title: 'devstore',
+  description: 'Sua loja oficial Dev.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt" className={inter.variable}>
+      <body>{children}</body>
+    </html>
+  )
+}
