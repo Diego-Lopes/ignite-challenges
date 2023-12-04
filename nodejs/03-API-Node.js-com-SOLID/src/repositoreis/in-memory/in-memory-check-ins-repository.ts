3f2b8d91a1a93b975f198e0b9a96db5dd4ac0a07
Diego-Lopes <diegolopes2009@hotmail.com>
2023-12-04 16:53:33 -0400

    ♻️`refactore`: modification components, new search method added

--- nodejs/03-API-Node.js-com-SOLID/src/repositoreis/in-memory/in-memory-check-ins-repository.ts
+++ nodejs/03-API-Node.js-com-SOLID/src/repositoreis/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,20 @@ import dayjs from 'dayjs'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date(),
+    }
+
+    this.items.push(checkIn)
+
+    return checkIn
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date')
     // ele retorna 2023-11-21T00:00:00
@@ -28,17 +42,9 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkInSameDate
   }
 
-  async create(data: Prisma.CheckInUncheckedCreateInput) {
-    const checkIn = {
-      id: randomUUID(),
-      user_id: data.user_id,
-      gym_id: data.gym_id,
-      validated_at: data.validated_at ? new Date(data.validated_at) : null,
-      created_at: new Date(),
-    }
-
-    this.items.push(checkIn)
-
-    return checkIn
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
   }
 }
