db887902e75c9f8c3572b94636cca7ba8f7cf888
Diego-Lopes <diegolopes2009@hotmail.com>
2023-12-06 21:18:27 -0400

    ♻️`refactore`: added new function in memory search many

--- nodejs/03-API-Node.js-com-SOLID/src/repositoreis/in-memory/in-memory-gyms-repository.ts
+++ nodejs/03-API-Node.js-com-SOLID/src/repositoreis/in-memory/in-memory-gyms-repository.ts
@@ -29,4 +29,10 @@ export class InMemoryGymsRepository implements GymsRepository {
     this.items.push(gym)
     return gym
   }
+
+  async searchMany(query: string, page: number) {
+    return this.items
+      .filter((item) => item.title.includes(query))
+      .slice((page - 1) * 20, page * 20)
+  }
 }
