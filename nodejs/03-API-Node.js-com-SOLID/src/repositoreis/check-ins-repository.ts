c7fd4546cf11434ef8676df0b647f4bac4da6c6b
Diego-Lopes <diegolopes2009@hotmail.com>
2023-12-08 19:26:09 -0400

    ♻️`refactore`: modification component repository

--- nodejs/03-API-Node.js-com-SOLID/src/repositoreis/check-ins-repository.ts
+++ nodejs/03-API-Node.js-com-SOLID/src/repositoreis/check-ins-repository.ts
@@ -4,7 +4,7 @@ export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   save(checkIn: CheckIn): Promise<CheckIn>
   findById(id: string): Promise<CheckIn | null>
-  findByUserIdOnDate(userId: string, data: Date): Promise<CheckIn | null>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   countByUserId(userId: string): Promise<number>
 }
