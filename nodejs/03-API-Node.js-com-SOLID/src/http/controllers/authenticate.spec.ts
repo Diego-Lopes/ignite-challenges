9ce5c1d3f5eb0ea008971b24994117219a54aae2
Diego-Lopes <diegolopes2009@hotmail.com>
2023-12-22 14:41:32 -0400

    ♻️refactore: added test e2e in authenticate

--- nodejs/03-API-Node.js-com-SOLID/src/http/controllers/authenticate.spec.ts
+++ nodejs/03-API-Node.js-com-SOLID/src/http/controllers/authenticate.spec.ts
@@ -1,10 +1,41 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 // testando o hackerzinho de variável ambiente
 
-import { test } from 'vitest'
+// vamos utilizar a lib supertest para fazer requisições http sem está hospedado.
+import request from 'supertest'
+
+import { app } from '@/app'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 // eslint-disable-next-line prettier/prettier
-test('ok', () => { })
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready() // vamos escutar um evento, para saber se nosso servidor foi inicializado
+  })
+
+  afterAll(async () => {
+    await app.close() // vamos escutar e aguardar a aplicação fechar.
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example1.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example1.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
 
 /**
  * Quanto os teste da pasta http forem executado usem o
