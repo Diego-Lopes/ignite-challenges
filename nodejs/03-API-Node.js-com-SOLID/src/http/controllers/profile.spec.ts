4f90385c99b5ba5e8f8f2548355aeccca52b0b4d
Diego-Lopes <diegolopes2009@hotmail.com>
2023-12-22 23:24:47 -0400

    ✨feat: created test e2e for profile

---
+++ nodejs/03-API-Node.js-com-SOLID/src/http/controllers/profile.spec.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+// testando o hackerzinho de variável ambiente
+
+// vamos utilizar a lib supertest para fazer requisições http sem está hospedado.
+import request from 'supertest'
+
+import { app } from '@/app'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+// eslint-disable-next-line prettier/prettier
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready() // vamos escutar um evento, para saber se nosso servidor foi inicializado
+  })
+
+  afterAll(async () => {
+    await app.close() // vamos escutar e aguardar a aplicação fechar.
+  })
+
+  it('should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example1.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example1.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@example1.com',
+      }),
+    )
+  })
+})
+
+/**
+ * Quanto os teste da pasta http forem executado usem o
+ * ambiente que está na pasta prisma.
+ */
