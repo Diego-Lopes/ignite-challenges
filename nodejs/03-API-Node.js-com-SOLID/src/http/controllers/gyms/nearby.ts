605d46c030e7dc8d707e277ad141559e4f79d1b4
Diego-Lopes <diegolopes2009@hotmail.com>
2023-12-23 14:16:44 -0400

    ✨feat: created controllers nearby and serch of gym

---
+++ nodejs/03-API-Node.js-com-SOLID/src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { longitude, latitude } = nearbyGymsQuerySchema.parse(request.body)
+
+  /**
+   * Padrão de pattern Factory
+   */
+  // pegando nosso caso de uso makeCreate
+  const fetchNearbyGymUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
---
+++ nodejs/03-API-Node.js-com-SOLID/src/http/controllers/gyms/search.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+import { z } from 'zod'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    /**
+     * .coerce facilita a transformação de um string em outros
+     * tipos válidos como boolean, number.
+     */
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { q, page } = searchGymsQuerySchema.parse(request.body)
+
+  /**
+   * Padrão de pattern Factory
+   */
+  // pegando nosso caso de uso makeCreate
+  const searchGymUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymUseCase.execute({
+    query: q,
+    page,
+  })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
