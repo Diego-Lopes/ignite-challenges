a562e9593a7d2b314c87f4587970f5b18c795e9a
Diego-Lopes <diegolopes2009@hotmail.com>
2023-12-23 14:17:12 -0400

    ♻️refactore: added new routes for gym

--- nodejs/03-API-Node.js-com-SOLID/src/http/controllers/gyms/routes.ts
+++ nodejs/03-API-Node.js-com-SOLID/src/http/controllers/gyms/routes.ts
@@ -1,7 +1,16 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { search } from './search'
+import { nearby } from './nearby'
+import { create } from './create'
 
 export async function gymsRouters(app: FastifyInstance) {
   // no fastify addHook faz o papel de middlewares
   app.addHook('onRequest', verifyJWT) // todas as rotas vão chamar o validador de token jwt.
+
+  // criando as rotas
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
