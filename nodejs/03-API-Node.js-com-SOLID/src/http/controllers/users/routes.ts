13d77deabdba2b8ff09b39a88e9ddbef05294e3e
Diego-Lopes <diegolopes2009@hotmail.com>
2023-12-26 22:51:39 -0400

    ♻️refactore: added router refresh token

--- nodejs/03-API-Node.js-com-SOLID/src/http/controllers/users/routes.ts
+++ nodejs/03-API-Node.js-com-SOLID/src/http/controllers/users/routes.ts
@@ -3,13 +3,16 @@ import { register } from './register'
 import { profile } from './profile'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { authenticate } from './authenticate'
+import { refresh } from './refresh'
 
-export async function usersRouters(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   // rota criar um novo usuário
   app.post('/users', register)
 
   // fazer login na aplicação.
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /**
    * As rotas abaixo só poderem ser chamadas se o usuário estiver autenticado.
