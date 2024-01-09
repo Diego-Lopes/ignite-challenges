b23dbbf9b95f11b13b757995905088accf709b96
Diego-Lopes <diegolopes2009@hotmail.com>
2024-01-08 23:56:59 -0400

    🚚chore: moved component to a folder below

--- nodejs/03-API-Node.js-com-SOLID/Challenge/src/http/routes.ts
+++ nodejs/03-API-Node.js-com-SOLID/Challenge/src/http/routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { registerORG } from './controllers/register'
+import { registerORG } from './controllers/organizations/register'
 
 /**
  * @param app tipado com FastifyInstance faz com que podemos usar um register do fastify
