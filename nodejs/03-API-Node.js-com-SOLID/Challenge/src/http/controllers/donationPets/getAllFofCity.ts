7d31f8e68c43c72789a32bcd76b227b051af01df
Diego-Lopes <diegolopes2009@hotmail.com>
2024-01-27 19:41:07 -0400

    ✨feat: added, new params of filter

--- nodejs/03-API-Node.js-com-SOLID/Challenge/src/http/controllers/donationPets/getAllFofCity.ts
+++ nodejs/03-API-Node.js-com-SOLID/Challenge/src/http/controllers/donationPets/getAllFofCity.ts
@@ -1,5 +1,8 @@
 import { ErrorBlankField } from '@/use-cases/errors/error-blank-field'
+import { ErrorGeneric } from '@/use-cases/errors/error-generic'
+import { NotExistDonationPetByCharacteristic } from '@/use-cases/errors/not-exists-donation-pet-by-characteristic'
 import { NotExistDonationPetByCity } from '@/use-cases/errors/not-exists-donation-pet-by-city'
+import { makeGetAllDonationPetByCharacteristicUseCase } from '@/use-cases/factories/make-get-all-donation-pet-by-characteristic-use-case'
 import { makeGetAllDonationPetUseCase } from '@/use-cases/factories/make-get-all-donation-pet-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
@@ -9,8 +12,15 @@ export async function getAllDonationPets(
   reply: FastifyReply,
 ) {
   const registerDonationPetSchema = z.object({
-    city: z.string(),
+    city: z.string().trim().optional(),
+    q: z.string().trim().optional(),
   })
+
+  const { q } = registerDonationPetSchema.parse(request.query)
+  console.log(q)
+
+  const getAllDonationPetByCharacteristicUseCase =
+    makeGetAllDonationPetByCharacteristicUseCase()
 
   const { city } = registerDonationPetSchema.parse(request.query)
   console.log(city)
@@ -18,13 +28,30 @@ export async function getAllDonationPets(
   const getAllDonationPetUseCase = makeGetAllDonationPetUseCase()
 
   try {
-    const donationPets = await getAllDonationPetUseCase.execute({
-      city,
-    })
+    if (city !== undefined) {
+      const donationPets = await getAllDonationPetUseCase.execute({
+        city,
+      })
+      return reply.status(200).send(donationPets)
+    }
 
-    return reply.status(200).send(donationPets)
+    if (q !== undefined) {
+      const donationPetsCharacteristic =
+        await getAllDonationPetByCharacteristicUseCase.execute({
+          q,
+        })
+      return reply.status(200).send(donationPetsCharacteristic)
+    }
   } catch (error: any) {
+    if (error instanceof ErrorGeneric) {
+      return reply.status(404).send({ message: error.message })
+    }
+
     if (error instanceof NotExistDonationPetByCity) {
+      return reply.status(404).send({ message: error.message })
+    }
+
+    if (error instanceof NotExistDonationPetByCharacteristic) {
       return reply.status(404).send({ message: error.message })
     }
 
@@ -32,6 +59,6 @@ export async function getAllDonationPets(
       return reply.status(404).send({ message: error.message })
     }
 
-    return reply.status(404).send({ message: error.message })
+    return error
   }
 }
