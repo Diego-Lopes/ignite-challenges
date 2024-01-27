c2eaa02083e6bdec7f370c27629960e2e2644f0b
Diego-Lopes <diegolopes2009@hotmail.com>
2024-01-27 17:34:52 -0400

    ♻️refactore: added checking for empty sends and returning an error

--- nodejs/03-API-Node.js-com-SOLID/Challenge/src/use-cases/get-all-donation-pet-of-city.ts
+++ nodejs/03-API-Node.js-com-SOLID/Challenge/src/use-cases/get-all-donation-pet-of-city.ts
@@ -3,6 +3,7 @@
 import { DonationPetsRepository } from '@/repositories/donation-pet-interfaces-repository'
 import { DonationPet } from '@prisma/client'
 import { NotExistDonationPetByCity } from './errors/not-exists-donation-pet-by-city'
+import { ErrorBlankField } from './errors/error-blank-field'
 
 // entrada
 interface GetAllDonationPetOfCityUseCaseRequest {
@@ -20,7 +21,14 @@ export class GetAllDonationPetOfCityUseCase {
   async execute({
     city,
   }: GetAllDonationPetOfCityUseCaseRequest): Promise<GetAllDonationPetOfCityUseCaseResponse> {
+
+    if (!city) {
+      throw new ErrorBlankField()
+    }
+
+
     const donationPets = await this.donationPetsRepository.findAllDonationPetOfCity(city)
+
 
     if (donationPets.length === 0) {
       throw new NotExistDonationPetByCity()
