9c1310cc929075a8c6ff8a4ca9b26340a59661ee
Diego-Lopes <diegolopes2009@hotmail.com>
2024-01-29 22:00:12 -0400

    ♻️chore: added route session

--- nodejs/03-API-Node.js-com-SOLID/Challenge/src/http/controllers/organizations/routes.ts
+++ nodejs/03-API-Node.js-com-SOLID/Challenge/src/http/controllers/organizations/routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { registerORG } from './register'
+import { authenticate } from './authenticate'
 
 /**
  * @param app tipado com FastifyInstance faz com que podemos usar um register do fastify
@@ -7,4 +8,5 @@ import { registerORG } from './register'
 
 export async function organizationRoutes(app: FastifyInstance) {
   app.post('/organizations', registerORG)
+  app.post('/sessions', authenticate)
 }
