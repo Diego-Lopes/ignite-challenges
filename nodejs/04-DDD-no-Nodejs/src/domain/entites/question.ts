1817a1afaf82e8a8d5a59ffff84f3e965d8d3030
Diego-Lopes <diegolopes2009@hotmail.com>
2024-02-04 14:03:50 -0400

    ♻️chore: add value object of slug

--- nodejs/04-DDD-no-Nodejs/src/domain/entites/question.ts
+++ nodejs/04-DDD-no-Nodejs/src/domain/entites/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
   title: string
   content: string
+  slug: Slug
   authorId: string
 
 }
@@ -10,12 +12,14 @@ interface QuestionProps {
 export class Question {
   public id: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
 
   constructor(props: QuestionProps, id?: string,) {
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
     this.id = id ?? randomUUID()
   }
