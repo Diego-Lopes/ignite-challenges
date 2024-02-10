eb8d60e95ebc1f9bdf1b285761a57441508cea86
Diego-Lopes <diegolopes2009@hotmail.com>
2024-02-10 18:03:26 -0400

    ✨feat: implementation getters & setters and added lib dayjs

--- nodejs/04-DDD-no-Nodejs/src/domain/entites/answer.ts
+++ nodejs/04-DDD-no-Nodejs/src/domain/entites/answer.ts
@@ -12,6 +12,43 @@ interface AnswerProps {
 
 export class Answer extends Entity<AnswerProps> {
   /**
+   * Implementando os métodos Getters e Setters
+   */
+  get authorId() {
+    return this.props.authorId
+  }
+  get questionId() {
+    return this.props.questionId
+  }
+  get createdAt() {
+    return this.props.createdAt
+  }
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  //basicamento um resumo da resposta
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  /**
+   * Para usar setters tem que fazer a seguinte pergunta, faz sentido 
+   * mudar o nome do criador da resposta? Faz sentido mudar a data de criação?
+   * única coisa que faz sentido é mudar o conteúdo da resposta.
+   * e vamos também setar a data de modificação e chamar ele no content para dizer 
+   * quando foi alterado a resposta.
+   */
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+  /**
    * como estamos passando props da classe entity não precisamos das variáveis
    * publica da class answer e demais. 
    */
--- nodejs/04-DDD-no-Nodejs/src/domain/entites/question.ts
+++ nodejs/04-DDD-no-Nodejs/src/domain/entites/question.ts
@@ -2,10 +2,11 @@ import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
 import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Optional } from "../../core/types/optinal";
+import dayjs from "dayjs";
 
 interface QuestionProps {
   authorId: UniqueEntityID
-  bestAnswerId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
   slug: Slug
@@ -14,6 +15,74 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+
+  /**
+   * Implementando os métodos Getters e Setters
+   */
+  get authorId() {
+    return this.props.authorId
+  }
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+  get title() {
+    return this.props.title
+  }
+  get content() {
+    return this.props.content
+  }
+  get slug() {
+    return this.props.slug
+  }
+  get createdAt() {
+    return this.props.createdAt
+  }
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  //basicamento um resumo da resposta
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  /**
+   * Para usar setters tem que fazer a seguinte pergunta, faz sentido 
+   * mudar o nome do criador da resposta? Faz sentido mudar a data de criação?
+   * única coisa que faz sentido é mudar o conteúdo da resposta.
+   * e vamos também setar a data de modificação e chamar ele no content para dizer 
+   * quando foi alterado a resposta.
+   */
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  /**
+   * Criando uma lógica para atualizar o título e slug
+   */
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+
+    this.touch()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  // setar a melhor resposta.
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
   // public title: string
   // public slug: Slug
   // public content: string
@@ -45,9 +114,12 @@ export class Question extends Entity<QuestionProps> {
    * 
    * @description Abstraindo criação de entidade
    */
-  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
+  static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID
+  ) {
     const question = new Question({
       ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
       createdAt: new Date(),
     }, id)
 
