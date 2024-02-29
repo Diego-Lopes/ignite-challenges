810de79d8b995241717659e0c7999a53b7ab4805
Diego-Lopes <diegolopes2009@hotmail.com>
2024-02-28 23:18:36 -0400

    ✨feat: implementation use case delete question comment, test and make question comment

--- nodejs/04-DDD-no-Nodejs/test/repository/in-memory-question-comments-repository.ts
+++ nodejs/04-DDD-no-Nodejs/test/repository/in-memory-question-comments-repository.ts
@@ -4,6 +4,22 @@ import { QuestionComment } from '@/domain/forum/enterprise/entites/question-comm
 
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository {
+  async findById(id: string) {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex((item) => item.id === questionComment.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
   public items: QuestionComment[] = []
 
   async create(questionComment: QuestionComment) {
