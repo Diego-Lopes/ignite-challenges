47059613f8f7f6d79b17da0e9ff39a8c58f199bf
Diego-Lopes <diegolopes2009@hotmail.com>
2024-02-29 21:16:12 -0400

    ✨feat: created use case delete answer comment, test, in memory and make

--- nodejs/04-DDD-no-Nodejs/test/repository/in-memory-answer-comments-repository.ts
+++ nodejs/04-DDD-no-Nodejs/test/repository/in-memory-answer-comments-repository.ts
@@ -6,6 +6,22 @@ export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository {
   public items: AnswerComment[] = []
 
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex((item) => item.id === answerComment.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
   }
