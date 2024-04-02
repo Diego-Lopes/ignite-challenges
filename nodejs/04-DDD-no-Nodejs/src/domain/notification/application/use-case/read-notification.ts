fb95d933c0fab37e77214468c65281ecaebdac9b
Diego-Lopes <diegolopes2009@hotmail.com>
2024-04-01 23:46:28 -0400

    ✨feat: created read notification and components

---
+++ nodejs/04-DDD-no-Nodejs/src/domain/notification/application/use-case/read-notification.ts
@@ -0,0 +1,46 @@
+import { Either, left, right } from '@/core/either'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Notification } from '../../enterprise/entities/notification'
+import { NotificationsRepository } from '../repositories/notification-repositories'
+
+interface ReadNotificationUseCaseRequest {
+  notificationId: string
+  recipientId: string
+}
+
+type ReadNotificationUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    notification: Notification
+  }
+>
+
+export class ReadNotificationUseCase {
+  constructor(private notificationRepository: NotificationsRepository) { }
+
+  async execute({
+    recipientId,
+    notificationId,
+  }: ReadNotificationUseCaseRequest): Promise<ReadNotificationUseCaseResponse> {
+    const notification =
+      await this.notificationRepository.findById(notificationId)
+
+    if (!notification) {
+      return left(new ResourceNotFoundError())
+    }
+
+    if (recipientId !== notification.recipientId.toString()) {
+      return left(new NotAllowedError())
+    }
+    // marcando como lido a notificação.
+    notification.read()
+
+    // salvando no repository
+    await this.notificationRepository.save(notification)
+
+    return right({
+      notification,
+    })
+  }
+}
