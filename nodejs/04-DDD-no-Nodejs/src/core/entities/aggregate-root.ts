c8a4342707450655a92a25bda2b62b03b82e5a01
Diego-Lopes <diegolopes2009@hotmail.com>
2024-04-11 21:16:17 -0400

    🚚replace: moved file aggregate root

--- nodejs/04-DDD-no-Nodejs/src/core/entities/aggregate-root.ts
+++ nodejs/04-DDD-no-Nodejs/src/core/entities/aggregate-root.ts
@@ -1,5 +1,23 @@
-import { Entity } from './entity'
-/**
- * Toda entidade aggregate extende de entidade Entity
- */
-export abstract class AggregateRoot<Props> extends Entity<Props> { }
+import { Entity } from '../entities/entity'
+import { DomainEvent } from '../event/domain-event'
+import { DomainEvents } from '../event/domain-events'
+
+export abstract class AggregateRoot<Props> extends Entity<Props> {
+  private _domainEvents: DomainEvent[] = []
+
+  get domainEvents(): DomainEvent[] {
+    return this._domainEvents
+  }
+
+  // dispara ele apartir das classes que herdão.
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    // este função vai pre disparar os eventos.
+    this._domainEvents.push(domainEvent)
+
+    DomainEvents.markAggregateForDispatch(this)
+  }
+
+  public clearEvents() {
+    this._domainEvents = []
+  }
+}
