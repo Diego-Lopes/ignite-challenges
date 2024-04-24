47bc803d16d3a2e6a54b6282c0e0ba1a80e2a741
Diego-Lopes <diegolopes2009@hotmail.com>
2024-04-24 12:59:43 -0400

    ✨feat: added middleware (UsePipes)

--- nodejs/05-Projeto-com-Nest/Projeto-05/src/controllers/create-account.controller.ts
+++ nodejs/05-Projeto-com-Nest/Projeto-05/src/controllers/create-account.controller.ts
@@ -4,16 +4,29 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
 import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) { }
+
   @Post()
   @HttpCode(201) // podemos forçar um tipo de http code
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema)) // UsePipes é um midelware que intercepta e valida os dados com zod.
+  async handle(@Body() body: CreateAccountBodySchema) {
     /**
      * para chamar os parâmetros de req, no nest usamod decorations
      * como queremos body da requisição usamos @Body logo em seguida
