ce1952d048d46240b090a67b6d3ea7ae726b12f9
Diego-Lopes <diegolopes2009@hotmail.com>
2024-04-24 10:20:59 -0400

    ✨feat: created path controllers

---
+++ nodejs/05-Projeto-com-Nest/Projeto-05/src/controllers/create-account.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) { }
+  @Post()
+  @HttpCode(201) // podemos forçar um tipo de http code
+  async handle(@Body() body: any) {
+    /**
+     * para chamar os parâmetros de req, no nest usamod decorations
+     * como queremos body da requisição usamos @Body logo em seguida
+     * passamos o resultado de @body para uma variável e podemos tipar essa variável.
+     */
+
+    const { name, email, password } = body
+
+    const data = {
+      name,
+      email,
+      password,
+    }
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      )
+    }
+
+    await this.prisma.user.create({
+      data,
+    })
+  }
+}
