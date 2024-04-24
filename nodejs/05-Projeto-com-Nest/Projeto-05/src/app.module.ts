dacb148c79fa451a44746a1638dab0cc07c19166
Diego-Lopes <diegolopes2009@hotmail.com>
2024-04-24 16:34:57 -0400

    📦lib: added library @nestjs/config and created file env.ts

--- nodejs/05-Projeto-com-Nest/Projeto-05/src/app.module.ts
+++ nodejs/05-Projeto-com-Nest/Projeto-05/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env), // validate é uma função que recebe uma propriedade do env
+      isGlobal: true, // essa opção deixa global para todos os modules da aplicação.
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
