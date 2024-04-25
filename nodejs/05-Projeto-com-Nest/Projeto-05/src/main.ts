9cbf33f55bb9575e908d88eecd23640638d468c4
Diego-Lopes <diegolopes2009@hotmail.com>
2024-04-25 08:37:43 -0400

    ♻️chore: finished config modules env

--- nodejs/05-Projeto-com-Nest/Projeto-05/src/main.ts
+++ nodejs/05-Projeto-com-Nest/Projeto-05/src/main.ts
@@ -1,14 +1,16 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
 
-  const configService = app.get(ConfigService)
-  const port = configService.get('PORT')
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  // o segundo parâmetros do generico config service tem wasvalited ele faz com que a variável de ambiente não fica undefined.
+  const port = configService.get('PORT', { infer: true })
 
   await app.listen(port)
 }
