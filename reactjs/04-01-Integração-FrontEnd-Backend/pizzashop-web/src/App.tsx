13c7a7909b9d54b60089b276de31e3180b0cb65f
Diego-Lopes <diegolopes2009@hotmail.com>
2024-04-30 17:53:23 -0400

    📦lib: added lib react router dom and implementation of libs

--- reactjs/04-01-Integração-FrontEnd-Backend/pizzashop-web/src/App.tsx
+++ reactjs/04-01-Integração-FrontEnd-Backend/pizzashop-web/src/App.tsx
@@ -1,11 +1,10 @@
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
 import './global.css'
+import { router } from './routes'
 
 export function App() {
 
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Enviar</Button>
-    </div>
+    <RouterProvider router={router} />
   )
 }
