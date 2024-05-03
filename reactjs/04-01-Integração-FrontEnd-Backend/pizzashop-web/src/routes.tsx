ffb38b7a13d6b50e1d55affc62b4b494002f7c6a
Diego-Lopes <diegolopes2009@hotmail.com>
2024-05-03 14:53:22 -0400

    ♻️chore: modification compnent in route /sign-in, added AuthLayout

--- reactjs/04-01-Integração-FrontEnd-Backend/pizzashop-web/src/routes.tsx
+++ reactjs/04-01-Integração-FrontEnd-Backend/pizzashop-web/src/routes.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 
@@ -13,7 +14,7 @@ export const router = createBrowserRouter([
   },
   {
     path: '/',
-    element: <AppLayout />,
+    element: <AuthLayout />,
     children: [{ path: '/sign-in', element: <SignIn />},]
   }
 
