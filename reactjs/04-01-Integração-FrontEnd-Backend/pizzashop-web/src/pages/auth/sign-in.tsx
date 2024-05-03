922c7a8499bacde3127f592845da8d66a93ae07b
Diego-Lopes <diegolopes2009@hotmail.com>
2024-05-03 15:09:25 -0400

    💄style: added styles css in auth/sign-in

--- reactjs/04-01-Integração-FrontEnd-Backend/pizzashop-web/src/pages/auth/sign-in.tsx
+++ reactjs/04-01-Integração-FrontEnd-Backend/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -4,7 +4,15 @@ export function SignIn() {
   return (
     <>
       <Helmet title="SignIn" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="w-[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold">Acessar painel</h1>
+            <p>Acompanhe suas vendas pelo painel do parceiro!</p>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </>
   )
 }
