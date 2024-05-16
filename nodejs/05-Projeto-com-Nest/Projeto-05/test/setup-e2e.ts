242e1097501337ca5069313770bde7571a602545
Diego-Lopes <diegolopes2009@hotmail.com>
2024-05-16 00:03:21 -0400

    ✨feat: implementation test e2e with vitest

---
+++ nodejs/05-Projeto-com-Nest/Projeto-05/test/setup-e2e.ts
@@ -0,0 +1,43 @@
+// isso é do vitest não do nestjs
+import { PrismaClient } from '@prisma/client'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  // fazendo uma verificação para verificar se está setado o url do banco de dados
+  // para podermos trocar o schema do bando.
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  // SETANDO O SCHEMA DO BANCO DE DADOS QUE VEM DO PARÂMETRO DA FUNÇÃO.
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+const schemaId = randomUUID()
+// Roda antes dos testes
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+  // feita a alteração databaseURL vamos sobrescrever no environment
+  process.env.DATABASE_URL = databaseURL
+
+  // comando para executar no terminal
+  // deploy ele não le as migration do projeto e sim o que já está no banco.
+  execSync('npx prisma migrate deploy')
+
+  console.log(databaseURL)
+})
+
+// Roda depois dos testes
+afterAll(async () => {
+  // após o teste precisamo deletar o schema criado.
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+  console.log('depois')
+})
--- nodejs/05-Projeto-com-Nest/Projeto-05/vitest.config.e2e.ts
+++ nodejs/05-Projeto-com-Nest/Projeto-05/vitest.config.e2e.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     include: ['**/*.e2e-spec.ts'], // pegar apenas extensão *e2e-spec.ts
     globals: true,
     root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
   },
   plugins: [
     tsConfigPaths(),
