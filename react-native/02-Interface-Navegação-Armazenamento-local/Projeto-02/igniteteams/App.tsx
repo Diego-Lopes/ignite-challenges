094bbf4d8204e631b98f52439c8726c378eee006
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-02 17:57:52 -0400

    📦lib: add font google of expo

--- react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/App.tsx
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/App.tsx
@@ -2,10 +2,18 @@ import { Groups } from "@screens/groups";
 import { ThemeProvider } from "styled-components";
 import { theme } from './src/theme';
 
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from "react-native";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+     {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }
