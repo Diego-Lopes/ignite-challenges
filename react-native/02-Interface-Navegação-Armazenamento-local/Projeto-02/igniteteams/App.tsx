868e9dc46db55f31488e63761eb02ded4de32363
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-02 17:31:17 -0400

    ✨feat: create theme of styled component

--- react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/App.tsx
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/App.tsx
@@ -1,7 +1,11 @@
 import { Groups } from "@screens/groups";
+import { ThemeProvider } from "styled-components";
+import defaultTheme from "./src/theme/index";
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={defaultTheme}>
+      <Groups />
+    </ThemeProvider>
   )
 }
