468da578fe74f539fa8ab39afc9f757e9a78afe7
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-02 15:28:34 -0400

    🧱ci: create project base

---
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/App.tsx
@@ -0,0 +1,7 @@
+import { Groups } from "@screens/groups";
+
+export default function App() {
+  return (
+    <Groups />
+  )
+}
---
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/screens/groups/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { styles } from "./style";
+
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
