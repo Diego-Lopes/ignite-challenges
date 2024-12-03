2e7ef569de4ec52f6ec67b65f36a3dea711f8f4c
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-03 12:09:43 -0400

    ♻️chore: add structure of group list

--- react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/screens/groups/index.tsx
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/screens/groups/index.tsx
@@ -1,17 +1,28 @@
 import { GroupCard } from "@components/groupCard";
 import { Header } from "@components/header";
 import { Highlight } from "@components/highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import * as S from "./style";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da T.I", "Galera da R.H", "Galera da BackOffice"])
   return (
     <S.Container>
       <Header showBackButton />
       <Highlight title={"Tumas"} subTitle={"Jogue com a sua turma"} />
 
-      <GroupCard title="Galera da T.I" onPress={() => {}}/>
-      <GroupCard title="Galera da RH"/>
-      <GroupCard title="Galera da BackOffice"/>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
+
+
     </S.Container>
   );
 }
