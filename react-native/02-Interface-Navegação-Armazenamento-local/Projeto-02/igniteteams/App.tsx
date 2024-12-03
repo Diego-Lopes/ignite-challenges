285e43bfa2adfed194459250583321388556cb41
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-03 09:33:11 -0400

    💄style: add style in status bar

--- react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/App.tsx
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "styled-components";
 
 import { Loading } from "@components/loading";
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
 import { theme } from "src/theme";
 
 export default function App() {
@@ -14,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+       barStyle={"default"}
+       backgroundColor={"transparent"}
+       translucent //começa no topo do dispositivo
+      />
      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
