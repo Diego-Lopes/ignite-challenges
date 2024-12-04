d121a05e354210a9463444fb70ee3f5e76948af3
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-04 12:07:45 -0400

    ✨feat: create navigation routes

---
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/routes/app.route.tsx
@@ -0,0 +1,27 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Groups } from "@screens/groups";
+import { NewGroup } from "@screens/newGroup";
+import { Players } from "@screens/players";
+
+// estratégia de navegação, createNativeStackNavigator
+const { Navigator, Screen } = createNativeStackNavigator() 
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen
+        name="groups" // crio o nome da rota
+        component={Groups} // chamo que vai ser renderizado na rota.
+      />
+      <Screen
+        name="new" // crio o nome da rota
+        component={NewGroup} // chamo que vai ser renderizado na rota.
+      />
+      <Screen
+        name="players" // crio o nome da rota
+        component={Players} // chamo que vai ser renderizado na rota.
+      />
+
+    </Navigator>
+  )
+}
