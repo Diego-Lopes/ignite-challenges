afec620953b2a6e7061b8936f7d99963e83f2283
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-04 09:42:09 -0400

    ♻️refactore: add count team and flatList

--- react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/screens/players/index.tsx
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/screens/players/index.tsx
@@ -3,25 +3,45 @@ import { Filter } from "@components/filter";
 import { Header } from "@components/header";
 import { Highlight } from "@components/highlight";
 import { Input } from "@components/input";
-import { Container, Form } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState<string>('')
+  const [coutnPlayers, setCountPlayers] = useState([])
   return (
     <Container>
       <Header showBackButton />
-      <Highlight 
+      <Highlight
         title="Nome da turma"
         subTitle="Adicione a galera e separe os times"
       />
-     <Form>
-      <Input 
+      <Form>
+        <Input
           placeholder="Nome da pessoa"
           autoCorrect={false}
-          
+
         />
-      <ButtonIcon icon="add" />
-     </Form>
-     <Filter title={"Time A"} isActive/>
+        <ButtonIcon icon="add" />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['TIME A', 'TIME B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{coutnPlayers.length}</NumberOfPlayers>
+      </HeaderList>
+
     </Container>
   )
 }
