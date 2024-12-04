9d5654f6e41dbbcf2cdb1435804f73c5044f3694
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-04 16:07:44 -0400

    ♻️refactore: add function go back

--- react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/components/header/index.tsx
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/components/header/index.tsx
@@ -1,4 +1,5 @@
 import logoImage from '@assets/logo.png';
+import { useNavigation } from '@react-navigation/native';
 import { BackButton, BackIcon, Container, Logo } from "./style";
 
 
@@ -7,11 +8,16 @@ type HeaderProps = {
 }
 
 export function Header({ showBackButton = false }: HeaderProps) {
+  const navigation = useNavigation()
+  function handleGoBack() {
+    navigation.navigate('groups')
+  }
+
   return (
     <Container>
       {
         showBackButton &&
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       }
--- react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/screens/newGroup/index.tsx
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/screens/newGroup/index.tsx
@@ -2,9 +2,15 @@ import { Button } from "@components/button";
 import { Header } from "@components/header";
 import { Highlight } from "@components/highlight";
 import { Input } from "@components/input";
+import { useNavigation } from "@react-navigation/native";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const navegation = useNavigation()
+  function handleNew() {
+    navegation.navigate('players', { group: 'time a' })
+  }
+
   return(
     <Container>
       <Header showBackButton />
@@ -12,7 +18,7 @@ export function NewGroup() {
         <Icon />
         <Highlight title="Nova turma" subTitle="Crie a turma para adicionar as pessoas" />
         <Input placeholder="Adicione o nome da turma"/>
-        <Button title="Criar" style={{ marginTop: 20 }}/>
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew}/>
       </Content>
     </Container>
   )
