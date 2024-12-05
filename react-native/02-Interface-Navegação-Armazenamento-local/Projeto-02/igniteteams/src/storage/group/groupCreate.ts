5f9f6de1d771eb2c4238b7ffbfdbc47cee0c6145
Diego-Lopes <diegolopes2009@hotmail.com>
2024-12-05 08:58:13 -0400

    ✨feat: create class error

--- react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/storage/group/groupCreate.ts
+++ react-native/02-Interface-Navegação-Armazenamento-local/Projeto-02/igniteteams/src/storage/group/groupCreate.ts
@@ -1,10 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { GROUP_COLLECTION } from "@storage/storageConfig"
+import { AppError } from "@utils/appError"
 import { groupGetAll } from "./groupGetAll"
 
 export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await groupGetAll()
+
+    const groupAlreadyExists = storedGroups.includes(newGroup)
+
+    if (groupAlreadyExists) {
+      throw new AppError("Já existe um grupo cadastrado com esse nome.")
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup])
 
