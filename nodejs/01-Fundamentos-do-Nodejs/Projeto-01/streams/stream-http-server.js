8d1339fbdd9d856e4148944bc3181f900794764a
Diego Lopes <diegolopes2009@hotmail.com>
2025-08-18 08:35:22 -0400

    ♻️chore: added new anotations

--- nodejs/01-Fundamentos-do-Nodejs/Projeto-01/streams/stream-http-server.js
+++ nodejs/01-Fundamentos-do-Nodejs/Projeto-01/streams/stream-http-server.js
@@ -12,20 +12,28 @@ class InverseNumberStream extends Transform {
 // req => ReadableStream
 // res => WritableStream
 
-const server = http.createServer(async (req, res) => {
-  const buffers = []
+const server = http.createServer((req, res) => {
+  return req.pipe(new InverseNumberStream()).pipe(res)
+})
 
-  for await(const chunk of req) {
+const server1 = http.createServer(async (req, res) => {
+  const buffers = []
+  console.log('Iniciado o recebimento do dado em stream...');
+
+  for await (const chunk of req) {
+    console.log('Recebendo dados da stream...');
+
     buffers.push(chunk)
   }
+  console.log('Finalizado o recebimento do dado stream...');
 
-  const fullStreamContent = Buffer.concat(buffers).toString()
+  const fullStreamContent = Buffer.concat(buffers)
 
   console.log(fullStreamContent);
 
-  return res.end(fullStreamContent) 
+  return res.end(fullStreamContent)
 
-  return req.pipe(new InverseNumberStream()).pipe(res);
+  // return req.pipe(new InverseNumberStream()).pipe(res);
 });
 
-server.listen(3334);
+server1.listen(3334);
